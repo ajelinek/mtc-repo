506589b240d25f81b0fcab9182b6705c3244eda1
ajelinek <ajelinek@users.noreply.github.com>
2023-05-10 23:39:05 +0000

    added ports to the dev container and created a single start command

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,10 @@ import { mountTRPCServer } from "./server";
 const app = express();
 mountTRPCServer(app);
 
+app.use("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(2021);
 
 export type { AppRouterDefinition } from "./routers";
