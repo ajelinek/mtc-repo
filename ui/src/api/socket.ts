43f83866a76be7a64366a9b65403c63bcce4af31
ajelinek <ajelinek@gmail.com>
2023-05-11 09:20:59 -0500

    changed the url to use the window value

--- ui/src/api/socket.ts
+++ ui/src/api/socket.ts
@@ -1,5 +1,7 @@
 import io, { Socket } from 'socket.io-client'
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:2021')
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  `${window.location.hostname}:2021`
+)
 
 export default socket
--- ui/src/api/trpc.ts
+++ ui/src/api/trpc.ts
@@ -1,13 +1,9 @@
 import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client'
 import type { AppRouterDefinition } from 'api/src/index'
 
+const url = `${window.location.hostname}:2021/trpc`
 const client = createTRPCProxyClient<AppRouterDefinition>({
-  links: [
-    loggerLink(),
-    httpBatchLink({
-      url: 'http://localhost:2021/trpc'
-    })
-  ]
+  links: [loggerLink(), httpBatchLink({ url })]
 })
 
 export default client
