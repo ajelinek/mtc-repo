62936b11e4a78d80e5cdfae7f585771df7c70adc
Jordan Leeper <j.m.leep@gmail.com>
2023-05-11 10:04:42 -0500

    updates ui to show meeting in separate context

--- ui/src/pages/Meeting/index.tsx
+++ ui/src/pages/Meeting/index.tsx
@@ -1,7 +1,22 @@
 import { useParams } from '@solidjs/router'
+import TimeDisplay from '../../components/Time'
+import { startTimer, timerSignal } from '../../store'
+import './style.scss'
 
 export default function Meeting() {
   const { id } = useParams<{ id: string }>()
 
-  return <h2>Meeting {id}</h2>
+  return (
+    <div class="content">
+      <h2>Meeting {id}</h2>
+      <button class="button1" onClick={() => startTimer()}>
+        Start
+      </button>
+      <div class="debug">
+        <p>
+          Timer: <TimeDisplay seconds={timerSignal.value} />
+        </p>
+      </div>
+    </div>
+  )
 }
